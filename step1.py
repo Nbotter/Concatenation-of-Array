74b9d77cb8515fb9b31d8e04f0c68dcc56d58b3a
Nbotter <tarenx3@gmail.com>
2026-04-30 15:27:16 +0900

    Implement method to convert binary linked list to int

---
+++ step1.py
@@ -0,0 +1,11 @@
+'''
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        result = 0
+        node = head
+        while node is not None:
+            result = result * 2 + node.val
+            node = node.next
+
+        return result
+'''
